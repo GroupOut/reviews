d15e72a8c61e23133983c021d211c89adc217c5d
Alec Hyde <alechyde91@gmail.com>
2018-07-28 02:55:45 -0700

    reformatted tests to use es5

--- __tests__/API.test.js
+++ __tests__/API.test.js
@@ -1,18 +1,18 @@
 
 var $ = require('jquery');
 
-describe('API get reviews test', () => {
-  it('returns the correct star score for the first review of deal 90', (done) => {
+describe('API get reviews test', function () {
+  it('returns the correct star score for the first review of deal 90', function (done) {
     $.ajax({
       url: 'http://localhost:3004/deal/90/reviews',
       type: 'GET',
-      success: (result) => {
+      success: function (result) {
         let firstReview = result[0];
         expect(firstReview.review_score).toEqual(5);
         done();
       }
     })
-    .catch(err => {
+    .catch(function (err) {
       console.log(err);
     });
   });
