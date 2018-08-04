04f550e93f96e2400fd91f63f6ecd65635053367
Alec Hyde <alechyde91@gmail.com>
2018-08-04 16:53:53 -0700

    changing user in database connect

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   host: process.env.RDS_HOST || 'ec2-34-222-46-246.us-west-2.compute.amazonaws.com',
   port: 3306,
-  user: 'user',
+  user: 'root',
   password: 'password',
   database: 'Customer_Reviews'
 };
