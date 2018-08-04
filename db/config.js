ce27f717da9d76c60a3da104069fe6998d06575b
Alec Hyde <alechyde91@gmail.com>
2018-08-04 16:51:03 -0700

    trying another host

--- db/config.js
+++ db/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  host: process.env.RDS_HOST || '34.222.46.246' || '172.31.25.171' || 'ip-172-31-25-171.us-west-2.compute.internal' || 'ec2-34-222-46-246.us-west-2.compute.amazonaws.com' || 'r-00516295df569210a',
-  user: 'root',
+  host: process.env.RDS_HOST || 'ec2-34-222-46-246.us-west-2.compute.amazonaws.com',
+  port: 3306,
+  user: 'user',
   password: 'password',
   database: 'Customer_Reviews'
 };
