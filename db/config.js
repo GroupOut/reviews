32257764b609069d2387e878dca72304f5a9b28d
Alec Hyde <alechyde91@gmail.com>
2018-08-04 16:18:21 -0700

    changed db host

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: process.env.RDS_HOST || 'ec2-34-222-46-246.us-west-2.compute.amazonaws.com',
+  host: process.env.RDS_HOST || '34.222.46.246',
   user: 'root',
   password: 'password',
   database: 'Customer_Reviews'
